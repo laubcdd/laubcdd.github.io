12c1a91e3eb8f6b1a30f36c3ed682d5a9ebb406a
laubcdd <84852661+laubcdd@users.noreply.github.com>
2023-11-06 19:05:07 +0800

    Add files via upload

---
+++ js/random_test.js
@@ -0,0 +1,5 @@
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+
