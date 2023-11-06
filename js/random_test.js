e44cc487bb6bc13122d733978c35dcf1e87d4f79
laubcdd <84852661+laubcdd@users.noreply.github.com>
2023-11-06 20:46:11 +0800

    Update random_test.js

--- js/random_test.js
+++ js/random_test.js
@@ -1,4 +1,4 @@
-var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!'];
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 发现提示的最好办法是时不时看资讯站的内容','Tips : 没啥'];
 var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
 
   document.write(Tips) ;
