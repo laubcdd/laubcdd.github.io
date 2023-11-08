61c3dc5963089d2e21644d5b13f4757220fd2dd7
laubcdd <84852661+laubcdd@users.noreply.github.com>
2023-11-08 20:19:01 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,137 @@
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+var all_tips = ['Tips var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+) ;
+    var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+    var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+    
+      document.write(Tips) ;
+      var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+      var Tips = all_tips[var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.flvar all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+oor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+.floor(Math.random()*all_tips.length)];
+      
+        document.write(Tips) ;
+      : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+
+  document.write(Tips) ;
+  var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+  var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+  
+    document.write(Tips) ;
+    var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+    var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+    
+      document.write(Tips) ;
+      var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+      var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+      
+        document.write(Tips) ;
+        var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！'];
+        var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
+        
+          document.write(Tips) ;
+                    
+  document.write(Tips) ;
