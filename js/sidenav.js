1f2c8f2b5e36fa7f9df87ee9dab54fea0fe6a001
laubcdd <84852661+laubcdd@users.noreply.github.com>
2023-11-08 21:13:57 +0800

    Add files via upload

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,9 +1,9 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-  document.getElementById("laubcdd").style.width = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("laubcdd").style.width = "0";
+  document.getElementById("mySidenav").style.width = "0";
 }
