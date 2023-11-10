0dbed3e68781ed118b241e6168f34eb13f6c92be
laubcdd <84852661+laubcdd@users.noreply.github.com>
2023-11-10 20:17:05 +0800

    Add files via upload

--- js/random_test.js
+++ js/random_test.js
@@ -1,4 +1,4 @@
-var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！','Tips : 众所周知:枫乔站长更新资讯站绝对头疼'];
+var all_tips = ['Tips : 1145141919810', 'Tips : 每日一杯奶，健康音游人', 'Tips : SB KohaD!!!','Tips : This is not a tips!','Tips : 没啥','Tips : 发现新提示的最好办法就是微笑着面对他，加油，奥里给！(bushi)','Tips : 发现新提示的最好办法是时不时看资讯站的内容！','Tips : 鸡汤来喽！','Tips : 众所周知:枫乔站长更新资讯站绝对头疼','Tips : 台长似乎对建车站这方面有着独特的见解...还是避开他建站为妙...否则...?'];
 var Tips = all_tips[Math.floor(Math.random()*all_tips.length)];
 
   document.write(Tips) ;
