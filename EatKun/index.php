dc011b60203a5eeab5b5cd2d24a7112ef2d7fddd
laubcdd <84852661+laubcdd@users.noreply.github.com>
2023-11-28 21:05:44 +0800

    Add files via upload

--- EatKun/index.php
+++ EatKun/index.php
@@ -40,6 +40,7 @@
                 <button type="button" class="btn btn-secondary btn-lg" id="replay" onclick="replayBtn()" data-i18n="again">AGAIN-I18N</button>
                 <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.reload()" data-i18n="home">HOME-I18N</button>
                 <button type="button" class="btn btn-secondary btn-lg" onclick="goRank()" data-i18n="rank">RANK-I18N</button>
+                <button type="button" class="btn btn-secondary btn-lg" onclick="window.location.href='https://laubcdd.github.io/'" data-i18n="index">INDEX-I18N</button>
             </div>
     </div>
     </div>
